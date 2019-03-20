4fad5ef80adfec71ecc7fa875dccba2da0aeec73
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-03-20 14:53:13 -0400

    Al got rid of the conflict

--- 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,9 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-<<<<<<< HEAD
+
 print("One Direction - Story of my life")
-=======
-print("One Direction - Beautiful")
-print("^Ewww")
->>>>>>> test
+
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.

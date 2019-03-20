39d5ce2b700491dfdf553b7aabbc30ae7626ab8e
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-03-20 14:50:12 -0400

    Al changed to demonstrate the branching feature

--- 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-print("One Direction - Beautiful")
+print("One Direction - Story of my life")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.

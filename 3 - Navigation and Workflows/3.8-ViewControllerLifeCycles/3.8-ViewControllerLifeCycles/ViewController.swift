3e6d73c1be7a80ddc7e1f0f9ec6d9d7273386b6b
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-03-21 19:38:43 -0400

    //practice with the four different types of view event methods, printing statements based on their applications

--- 3 - Navigation and Workflows/3.8-ViewControllerLifeCycles/3.8-ViewControllerLifeCycles/ViewController.swift
+++ 3 - Navigation and Workflows/3.8-ViewControllerLifeCycles/3.8-ViewControllerLifeCycles/ViewController.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //VIEW DID LOAD - good for work that needs to be updated once, like font, text, color
     override func viewDidLoad() {
         super.viewDidLoad()
-        //good for work that needs to be updated once, like font, text, color 
         print("View Controller - View Did Load")
     }
+    
+    //VIEW WILL APPEAR - setting up work that needs to be performed for a few seconds before the view is loaded
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated) //viewWillAppear(_:) requires parameter "animated", thus passed in subclass
+        print("View Controller - View Will Appear")
+        
+    }
+    
+    //VIEW DID APPEAR - If your works needs to be performed each time the view appears, and needs a couple of seconds
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View Controller - View DID Appear")
+    }
 
+    //VIEW WILL DISAPPEAR - Called before the view disappears from the screen
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View Controller - View Will Disappear")
+    }
+    //VIEW DID DISAPPEAR - - Called after the view disappears from the screen
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View Controller - View DID Disappear")
+        
+    }
 
 }
 

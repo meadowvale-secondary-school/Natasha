18ccb9c9d0d32104a3b9b68862d62607a7f80066
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-03-28 11:11:19 -0400

    //Starting to work on the user interface of the Personality Quiz including the segues, navigation bars, and controls to be used when programming the quiz

---
+++ 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Student04 on 2019-03-28.
+//  Copyright © 2019 Student04. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

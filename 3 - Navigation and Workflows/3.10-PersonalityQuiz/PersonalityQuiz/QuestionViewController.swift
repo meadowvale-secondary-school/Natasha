0b22f6b43016681ca1e1b64bc0d68f997ba851ea
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-03-29 20:20:52 -0400

    //Set up case information for QuestionsData, created questions initalizer in order to assign as Question Collection to include question instances and answer property to include Answer collection for the three types of questions (single, multiple, ranged).
    Also started to work on the QuestionViewController to set up outlets for the questions stacks and corresponding labels/buttons. Set up updateUI to update whenever new question presented in view. Created currentQuestions and currentAnswer in order to update question type presented in view based on index value.

--- 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -9,22 +9,70 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    
+    //***OUTLETS*** (update button and labels)
+    @IBOutlet weak var questionLabel: UILabel! //main label at top of Questions view
+    
+    //change the visiblity of the stacks/question types
+    //SINGLE STACK
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
+    //MULTIPLE STACK
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
+    
+    //RANGE STACK
+    @IBOutlet weak var rangeStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+  
+    var questionIndex = 0   //var to keep track of questions displayed using integer value of collection (Starts at 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()//called before each question (VIEW) for title and visiblity of stack
     }
+    func updateUI() {
+        singleStackView.isHidden = true //set to hidden immediately
+        multipleStackView.isHidden = true
+        rangeStackView.isHidden = true
+        
+        //CURRENT QUESTIONS AND ANSWERS
+        let currentQuestion = questions[questionIndex]
+        //currentQuestion is set to questions collection, with Index number chosen based on int value
+        let currentAnswers = currentQuestion.answers
+        //set to questions for instances of Question with property of Answers
+        let totalProgress = Float(questionIndex) / Float(questions.count) //calcuate percentage progress by dividing questionIndex by total number of questions
+
+        
+        navigationItem.title = "Question #\(questionIndex+1)" //Displays in nav bar, with question correspoinding to index number
+        questionLabel.text = currentQuestion.text //refers to answers property text, question titles
+        questionProgressView.setProgress(totalProgress, animated: true) //UIProgressView Initalizer for setProgress function for float parameter (between 0.0 and 1.0) and animation parameter
+        
+        
+        switch currentQuestion.type { //refers to questionIndex for current Q's #
+        case .single:
+            singleStackView.isHidden = false //#1
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangeStackView.isHidden = false
+        }
+    }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
+    
 }
--- 3 - Navigation and Workflows/3.10-PersonalityQuiz/QuestionData.swift
+++ 3 - Navigation and Workflows/3.10-PersonalityQuiz/QuestionData.swift
@@ -24,10 +24,52 @@ enum ResponseType {
 //ANSWER
 struct Answer {
     var text: String //string to display to user result from enum
-    var type: ItemType //ties the answer to specific type
+    var type: itemType //ties the answer to specific type
 }
 
-enum ItemType {
-    case globe = globePersonalityResult
+
+enum itemType: Character {
+    case book = "📚", globe = "🌎",beaker = "🧪", bell = "🛎"
+    
+    var definition: String { //property presents as a label on the results screen
+        switch self { //used for enum "itemType"
+        case .book:
+            return "You are the kind of learner that always reaches for the book--or Google, in order to satisfy your questions and curiousity. You have random facts stored in your head from all that you've read, which certainly comes in handy for trivia night!"
+        case .globe:
+            return "You are a learner through experience. You much rather gain insight from those who are more experienced, and likes to see things from others' perspective. Travelling around the world would be a dream for this kind of learner!"
+            
+        case .beaker:
+            return "You are a scientist, in the sense that you learn by trial and error. You test out new things, and are not afraid to make mistakes. A lot times, the things that you learn are by accident, or are the 'biproduct' of your curiousity!"
+            
+        case .bell:
+            return "You are the kind of learner you likes to learn things as the come. When opportunities arise, you're curiousity is 'rung' by asking questions and sharing your response. You like to work with others in order to develop your understanding, and thrive in group discussions!"
+            
+        }
+        
+    }
 }
+//Any Question you create stored in collection from Question and Answer structures as instancees
+var questions: [Question] = [
+    Question(text: "What is your favorite kind of drink?", type: .single, answers: [
+        Answer(text: "Tea", type: .book), //answer Structure from within "answers" array type
+        Answer(text: "Coffee", type: .globe),
+        Answer(text: "Kombucha", type: .beaker),
+        Answer(text: "Chocolate Milk", type: .bell)
+        ]), //instance of Question, with answer's array type
+    Question(text: "Which activities do you like?", type: .multiple, answers: [
+        Answer(text: "Reading", type: .book),
+        Answer(text: "Travelling", type: .globe),
+        Answer(text: "Puzzles", type: .beaker),
+        Answer(text: "Board Games", type: .bell)
+        ]),
+    Question(text: "How much do you enjoy podcasts?", type: .ranged, answers: [
+        Answer(text: "I dislike them", type: .book),
+        Answer(text: "They're ok sometimes", type: .beaker),
+        Answer(text: "I like to listen to them", type: .bell),
+        Answer(text: "I can listen all day to them!", type: .globe)
+        ])
+    
+]
+
+
 

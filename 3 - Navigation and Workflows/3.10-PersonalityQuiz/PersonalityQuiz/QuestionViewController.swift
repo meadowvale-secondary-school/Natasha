e80ccc4500143ed2b9c6cc6e08a48fd8ab9faa9c
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-04-01 19:06:19 -0400

    //started to add control flow for retrieving answers from different stack questions. Currently working on calculating and displaying the results from each of the questions and the user's answers, making use of the last view controller - ResultsViewController to handle the segue and display the user's personality result.

--- 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -28,14 +28,22 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multiLabel3: UILabel!
     @IBOutlet weak var multiLabel4: UILabel!
     
+    //multiple stack switches
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
+    
+    
     //RANGE STACK
     @IBOutlet weak var rangeStackView: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
     
-    @IBOutlet weak var questionProgressView: UIProgressView!
+    @IBOutlet weak var rangedSlider: UISlider!
     
-  
+    @IBOutlet weak var questionProgressView: UIProgressView!
+
     var questionIndex = 0   //var to keep track of questions displayed using integer value of collection (Starts at 0)
     
     override func viewDidLoad() {
@@ -83,6 +91,10 @@ class QuestionViewController: UIViewController {
     //MULTI-ANSWER RESPONSE
     func updatesMultipleStack(using answers: [Answer]) {
         multipleStackView.isHidden = false
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiSwitch3.isOn = false
+        multiSwitch4.isOn = false
         multiLabel1.text = answers[0].text //takes Answers property of text from collection 
         multiLabel2.text = answers[1].text
         multiLabel3.text = answers[2].text
@@ -91,8 +103,77 @@ class QuestionViewController: UIViewController {
     //RANGED ANSWER RESPONSE
     func updateRangedStack(using answers: [Answer]){
         rangeStackView.isHidden = false
+        rangedSlider.setValue(0.5, animated: false)
         rangedLabel1.text = answers.first?.text //if collection is empty, set to nil
         rangedLabel2.text = answers.last?.text //no matter the amount of answers in ranged response, first and last prop. allow safe access of two Answer structs corresponding to labels
-        
     }
+    //**RETRIEVE ANSWERS WITH ACTIONS** - record the player's answers, move to next question, once all questions presented, move onto answer screen
+    
+    var answerChosen: [Answer] = [] //stores the player's answer in a collection
+    
+    //When one of the singleButtons are pressed:
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        switch sender { //checks which UIbutton corresponds
+        case singleButton1: ///singleButton1 chosen, equates to first answer selected
+            answerChosen.append(currentAnswers[0])
+        case singleButton2:
+            answerChosen.append(currentAnswers[1])
+        case singleButton3:
+            answerChosen.append(currentAnswers[2])
+        case singleButton4:
+            answerChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        nextQuestion()
+    }
+    
+    //When one or more multiple answer buttons are pressed:
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        //with if statements, able to append as many as 4 answers
+        if multiSwitch1.isOn {
+            answerChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answerChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answerChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answerChosen.append(currentAnswers[3])
+        }
+        nextQuestion()
+    }
+    //When ranged answer are selected, within range value for each
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        //convert slider value to array's index using equation, rounded to nearest integer (index value)
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1))) //set to int type in order to append in currentAnswers index
+        answerChosen.append(currentAnswers[index]) //appends caluated index integer using rangeSlider value
+        nextQuestion()
+    }
+    //***RESPOND TO ANSWERED QUESTIONS***
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count { //if the amount of questions answered is less than the amount of questions stored in Question, can continue to call to update the title and the proper stack view (method will use the new value of questionIndex to display next q)
+            updateUI()
+            
+        } else {
+            performSegue(withIdentifier: "ResultsSegue", sender: nil) //once all the questions have been completed, the segue between the questions and results view controller can take place
+        }
+    }
+    //**CALCULATE AND DISPLAY RESULTS**
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ResultsSegue" { //ensures segue is between ResultsSegue
+            let resultsViewController = segue.destination as! ResultsViewController
+            //downcast the destination property from UIViewController to ResultsViewController to access responses property
+            resultsViewController.responses = answerChosen
+        }
+    }
+    
 }
--- 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
+++ 3 - Navigation and Workflows/3.10-PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    var responses: [Answer]! //of Answer type and implicitly unwrapped so tha viewController can be displayed from storyboard even if the property has no value
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }

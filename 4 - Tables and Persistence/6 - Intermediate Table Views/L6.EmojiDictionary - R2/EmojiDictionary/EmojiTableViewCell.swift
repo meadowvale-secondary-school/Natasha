2ed66051c9f0fdee9542fdb7dbb6f10f909b10f9
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-04-15 23:47:56 -0400

    //started to being 4.6 Intermediate Table Views lesson, continuing from the emojiDictionary project (copied)  in order to add customizations to table views and their cells

---
+++ 4 - Tables and Persistence/6 - Intermediate Table Views/L6.EmojiDictionary - R2/EmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by Student04 on 2019-04-15.
+//  Copyright © 2019 Student04. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

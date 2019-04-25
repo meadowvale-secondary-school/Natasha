901f98423a77df43ea33277a2fe334e9ebd9da1c
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-04-25 08:36:47 -0400

    Working through lesson 4.8 System View Controllers making use of different types of view controllers for different applications.

---
+++ 4 - Tables and Persistence/8 - System View Controllers/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Student04 on 2019-04-24.
+//  Copyright © 2019 Student04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

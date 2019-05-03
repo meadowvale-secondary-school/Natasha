86b0697919362a98cd1e5516a533b70b7ea50748
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-05-03 08:58:42 -0400

    Completed working through first 4 sections of the to-do list app

--- 4 - Tables and Persistence/ToDoList/ToDoList/ToDoTableViewController.swift
+++ 4 - Tables and Persistence/ToDoList/ToDoList/ToDoTableViewController.swift
@@ -12,6 +12,8 @@ class ToDoTableViewController: UITableViewController {
            ToDo.loadSampleToDos()
         }
         
+        navigationItem.leftBarButtonItem = editButtonItem
+        
     }
     
    var todos = [ToDo]() //table view manages the collection of items - using an array of Todos
@@ -40,11 +42,33 @@ class ToDoTableViewController: UITableViewController {
         
     }
     
+//choose which cells are editable
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+//When the cell is swiped, a red delete button app
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete { //verify the Delete button triggered the method call
+            todos.remove(at: indexPath.row) //delete the model from the array
+            tableView.deleteRows(at: [indexPath], with: .fade) //as well as from the table view 
+            
+        }
+    }
+
+    @IBAction func unwindToToDoList(unwind: UIStoryboard) {
+        
+    }
+        
+        
+        
+    }
     
     
     
     
     
-    
-}
+
 

197eb7efaa4c9650a32a118eef2e2e2af2b99091
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-05-13 10:16:02 -0400

    Completed the 5.3 Animations practice with creating a music playlist animation

---
+++ 5 - Working With the Web/5.3-Animations/5.3-Animations/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  5.3-Animations
+//
+//  Created by Student04 on 2019-05-12.
+//  Copyright © 2019 Student04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//animate with duration
+    /*
+     UIView.animate(withDuration: 2.0) //changes properties
+     aView.alpha = 0.3
+ */
+    
+    
+}
+

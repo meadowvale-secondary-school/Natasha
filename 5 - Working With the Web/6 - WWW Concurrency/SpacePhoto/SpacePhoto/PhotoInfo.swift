dfdd2d2d891eb5c2a4e187f714985f1180daca22
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-05-14 10:24:58 -0400

    Re added 5.6 to proper branch for working with Concurrency and the web

---
+++ 5 - Working With the Web/6 - WWW Concurrency/SpacePhoto/SpacePhoto/PhotoInfo.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoInfo.swift
+//  SpacePhoto
+//
+//  Created by Student04 on 2019-05-14.
+//  Copyright © 2019 Student04. All rights reserved.
+//
+
+import Foundation
---
+++ 5 - Working With the Web/6 - WWW Concurrency/SpacePhoto/SpacePhoto/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Student04 on 2019-05-14.
+//  Copyright © 2019 Student04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

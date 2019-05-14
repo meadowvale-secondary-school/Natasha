d2e5a815460668789b78d35895281e689f65879d
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-05-14 10:22:20 -0400

    Accident placement of 5.6 file in 5.5 branch!

--- 5 - Working With the Web/6 - WWW Concurrency/SpacePhoto/SpacePhoto/ViewController.swift
+++ 5 - Working With the Web/6 - WWW Concurrency/SpacePhoto/SpacePhoto/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

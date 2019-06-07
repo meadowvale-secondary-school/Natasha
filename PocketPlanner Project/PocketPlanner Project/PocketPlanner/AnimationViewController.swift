1d337cf54261c967cb205a530f8ba238cae48bb6
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-06-07 08:45:34 -0400

    starting to add animations to the start view controller

--- PocketPlanner Project/PocketPlanner Project/PocketPlanner/AnimationViewController.swift
+++ PocketPlanner Project/PocketPlanner Project/PocketPlanner/AnimationViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class AnimationViewController: UIViewController {
 
+    @IBOutlet weak var oneCloud: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    func moveCloudRight(image: UIImage) {
-        let positionAnimation = CABasicAnimation(keyPath: "position")
-        
-        positionAnimation.fromValue = NSValue(cgPoint: CGPoint(x: image.center.x + 70, y: image.center.y))
-        positionAnimation.toValue = NSValue(cgPoint: CGPoint(x: image.center.x, y: label.center.y))
-        positionAnimation.duration = 0.2
-        
-        
-    }
+    //
+    //  startPageAnimation.swift
+    //  PocketPlanner
+    //
+    //  Created by Student04 on 2019-06-05.
+    //  Copyright © 2019 Student04. All rights reserved.
+    //
+    
 
 
 }

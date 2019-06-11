ec740b1d29b8bdf97857ceb3132ae64134e51182
Natasha Ahammed - Meadowvale SS (2562) <631811@pdsb.net>
2019-06-11 09:37:56 -0400

    Changes made to WWW files

--- 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/iTunesSearch.playground/Contents.swift
+++ 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/iTunesSearch.playground/Contents.swift
@@ -10,7 +10,7 @@ extension URL {
     func withQueries(_ queries: [String: String]) -> URL? {
         
         var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
-        components?.queryItems = queries.flatMap { URLQueryItem(name: $0.0, value: $0.1) }
+        components?.queryItems = queries.compactMap { URLQueryItem(name: $0.0, value: $0.1) }
         return components?.url
     }
 }
@@ -58,7 +58,7 @@ func fetchItems(matching query: [String: String], completion: @escaping ([StoreI
             let json = rawJSON as? [String: Any],
             let resultsArray = json["results"] as? [[String: Any]] {
             
-            let storeItems = resultsArray.flatMap { StoreItem(json: $0) }
+            let storeItems = resultsArray.compactMap { StoreItem(json: $0) }
             completion(storeItems)
             
         } else {
